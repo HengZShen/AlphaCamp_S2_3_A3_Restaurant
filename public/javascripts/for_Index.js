439a80283db02019db4e0948395a6ebbcc058ae0
HengZShen <r06621112@gmail.com>
2021-03-12 21:54:37 +0800

    Add css variables for DarkMode and Add JS for toggle Switch Listener

--- public/javascripts/for_Index.js
+++ public/javascripts/for_Index.js
@@ -1,7 +1,7 @@
 const loader = document.querySelector('.loader-container')
 const container = document.querySelector('.card-container')
 const searchBtn = document.querySelector('#search-button')
-
+const toggleSwitch = document.querySelector('input[type="checkbox"]')
 
 // loading
 searchBtn.addEventListener('click', loading)
@@ -17,6 +17,19 @@ function loading() {
 
 
 // Light Dark mode
+
+function switchMode(event) {
+  const checked = event.target.checked
+  if (checked) {
+    document.documentElement.setAttribute('data-mode', 'Dark')
+  } else {
+    document.documentElement.setAttribute('data-mode', 'Light')
+  }
+
+}
+
+
+toggleSwitch.addEventListener('change', switchMode)
 
 
 
