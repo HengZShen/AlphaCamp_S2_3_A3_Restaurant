b45f82991772d47308b405efc429979b34933fe3
HengZShen <r06621112@gmail.com>
2021-03-23 17:12:01 +0800

    refactor: modify code order in app.js

--- app.js
+++ app.js
@@ -1,35 +1,30 @@
-// Include modules and files
+// express server
 const express = require('express')
 const app = express()
 const port = 3000
 
-
-// bodyParser 
-const bodyParser = require('body-parser')
-app.use(bodyParser.urlencoded({ extended: true }))
-
-// Router 
-const routes = require('./routes')
-app.use(routes)
-
 // handlebars
 const exphbs = require('express-handlebars')
-
-
-// Restaurant model
-const Restaurant = require('./models/restaurant')
-
-
-// mongodb connect
-require('./config/mongoose')
 
 // set template engine
 app.engine('handlebars', exphbs({ defaultLayout: "main" }))
 app.set("view engine", 'handlebars')
 
-
 // set static files
 app.use(express.static('public'))
+
+// bodyParser 
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// mongodb connect
+require('./config/mongoose')
+
+// Router 
+const routes = require('./routes')
+app.use(routes)
+
+
 
 
 // start and listen server
