e8b3c0542eba0678f3066f895151731487bb925d
HengZShen <r06621112@gmail.com>
2021-04-18 16:17:30 +0800

    refactor: modify search.js

--- routes/modules/search.js
+++ routes/modules/search.js
@@ -4,52 +4,42 @@ const Restaurant = require('../../models/restaurant')
 const keyForSearch = require('../../public/javascripts/routes/search')
 const keyForSort = require('../../public/javascripts/routes/sort')
 
+
+function getQuery(name, category, rating, location) {
+  const query = Restaurant.find({
+    name: { $regex: name, $options: 'i' }, category: { $regex: category, $options: 'i' }, rating: { $gte: rating }, location: { $regex: location, $options: 'i' }
+  })
+  return query
+}
+
+function getPromise(query, nameOrient, ratingOrient) {
+  if (nameOrient) {
+    query = query.sort(nameOrient)
+
+  } else if (ratingOrient) {
+    query = query.sort(ratingOrient)
+  }
+  return query.lean().then()
+}
+
 router.get('/', (req, res) => {
 
   const { keyword, searchType, sortType
   } = req.query
   const { name, category, rating, location } = keyForSearch(searchType, keyword)
   const { nameOrient, ratingOrient } = keyForSort(sortType)
-  let document
 
-  console.log('0')
+  let query = getQuery(name, category, rating, location)
+  let promise = getPromise(query, nameOrient, ratingOrient)
 
-  // 評價 不可讀取數字外的內容
-  if (searchType === '評價') {
+  promise.then(restaurants => {
 
-    if (isNaN(Number(keyword))) {
+    if (restaurants.length === 0) {
       setTimeout(() => { res.render('noResult', { keyword, searchType }) }, 1000)
-      return
+    } else {
+      setTimeout(() => { res.render('index', { restaurants, keyword, searchType, sortType }) }, 1000)
     }
-  }
-
-  // query
-  document = Restaurant.find({
-    name: { $regex: name, $options: 'i' }, category: { $regex: category, $options: 'i' }, rating: { $gte: rating }, location: { $regex: location, $options: 'i' }
-  })
-
-  // console.log('1')
-  if (nameOrient) {
-    document = document.sort(nameOrient)
-    // console.log('name')
-  } else if (ratingOrient) {
-    document = document.sort(ratingOrient)
-    // console.log('rating')
-  }
-
-  // console.log('2')
-  document
-    .lean()
-    .then(restaurants => {
-      if (restaurants.length === 0) {
-        setTimeout(() => { res.render('noResult', { keyword, searchType }) }, 1000)
-      } else {
-        setTimeout(() => { res.render('index', { restaurants, keyword, searchType, sortType }) }, 1000)
-      }
-    })
-    .catch(error => console.log(error))
-
-  // console.log('3')
+  }).catch(error => console.log(error))
 })
 
 
